b1b05270ec66634bb8837ec93c3e81f2069a0ab5
Ziad <ziadd.essam03@gmail.com>
2025-08-11 23:56:04 +0200

    changed camera position

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(4, 5, 11);
 
+camera.position.set(11,5,11)
+
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
